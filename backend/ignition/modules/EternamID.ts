d1c238dcf947d90696a68872f6a6464495f61f8b
Cyril Fischer <github@cyril-fischer.fr>
2025-12-14 11:56:16 +0100

    edit: Backend - Ignition File

--- backend/ignition/modules/EternamID.ts
+++ backend/ignition/modules/EternamID.ts
@@ -1,9 +1,14 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("EternamIDModule", (m) => {
-    const deployer = m.getAccount(0);
-    const eternamID = m.contract("EternamID", [deployer]);
+export default buildModule("EternamID", (m) => {
+    const account = m.getAccount(0);
+    const teamWallet = m.getAccount(1);
+    const usdc = m.contract("MockERC20", ["USDC", "USDC", 6]);
+    m.call(usdc, "mint", [account, 200 * 10 ** 6]);
 
+    const eternamID = m.contract("EternamID", [teamWallet, usdc]);
 
-    return { eternamID };
+    m.call(usdc, "approve", [eternamID, 120 * 10 ** 6]);
+
+    return { usdc, eternamID };
 });
